861754cce5308b76ab4b89c1d91e24542b6205bf
Pranav Kochhar <pranavkochhar3198@gmail.com>
2022-02-24 13:06:53 +0530

    Update ex12.js

--- exercises/ex12.js
+++ exercises/ex12.js
@@ -29,3 +29,14 @@ const fn = async () => {
 //3   
 getPromise()
  .then(process, handleError);
+
+//Complete the following Task
+//There is a made up API URL (http://great.jokes/christmas) that you can call to a get list of Christmas jokes in JSON format.
+
+//Write an async function which takes an apiUrl and jokeId which returns a promise.
+//The data will need to be filtered to get the specified joke by id.
+//When you got the joke it should be accessible through a simple API of saySetup and sayPunchLine methods.
+
+//If a joke can't be found throw an error message in this format new Error('No jokes found id: {jokeId}').
+//Getting jokes from a another API URL may return a different data shape, throw this error message new Error('No jokes at url: {url}') for an unexpected shape.
+//Throw error in a promise style
