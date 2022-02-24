5fccdc276d3f49a1a37b4b1df272974305b1b03c
Pranav Kochhar <pranavkochhar3198@gmail.com>
2022-02-24 13:08:00 +0530

    Update ex12.sol.js

--- solutions/ex12.sol.js
+++ solutions/ex12.sol.js
@@ -32,3 +32,34 @@ const withTempDir = async (fn) => {
    getPromise()
  .then(process)
  .catch(handleError);
+
+//Task
+
+function sayJoke(apiUrl, jokeId) {
+    return fetch(apiUrl).then(response =>
+        response.json()
+    ).then(json => {
+        for (let joke of json.jokes) {
+            if (joke.id === jokeId) {
+                return {saySetup: _ => joke.setup, sayPunchLine: _ => joke.punchLine};
+            }
+        }
+        throw new Error(`No jokes found id: ${jokeId}`);
+    }).catch(error => {
+        throw (error instanceof TypeError)? new Error(`No jokes at url: ${apiUrl}`) : error
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
